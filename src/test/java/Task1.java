41973ecfba926fd71e4abff65ff71450af801eab
Ana Khurtsidze <anakhurtsidze@credo.ge>
2024-11-06 21:58:18 +0400

    Task1_ana

---
+++ src/test/java/Task1.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Eve", "Charlie", "Daniel");
+
+        List<String> filteredNames = names.stream().filter(name -> name.contains("e")).toList();
+
+
+       for(String name : filteredNames)
+           System.out.println(name);
+    }
+}
